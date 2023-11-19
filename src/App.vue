af2e2b9b790acf2aaf0aee678b5e614da3cab9f4
Muniox <muniox20@gmail.com>
2023-11-19 22:27:56 +0100

    Initial project files

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import Header from './components/Header.vue'
+import Balance from '@/components/Balance.vue'
+import IncomeExpenses from '@/components/IncomeExpenses.vue'
+import TransactionList from '@/components/transactionList.vue'
+import AddTransaction from '@/components/AddTransaction.vue'
+</script>
+
+<template>
+  <Header />
+  <div class="container">
+    <Balance />
+    <IncomeExpenses />
+    <TransactionList />
+    <AddTransaction />
+  </div>
+</template>
+
+<style scoped></style>
