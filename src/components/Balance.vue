f139f4e2e1f947984b9f19d95352b0acac54db35
Muniox <muniox20@gmail.com>
2023-11-22 12:59:52 +0100

    Transaction and Balance component updated

--- src/components/Balance.vue
+++ src/components/Balance.vue
@@ -1,8 +1,10 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps<{ total: number }>()
+</script>
 
 <template>
   <h4>Your Balance</h4>
-  <h1 id="balance">$0.00</h1>
+  <h1 id="balance">{{ total }}</h1>
 </template>
 
 <style scoped></style>
--- src/components/transactionList.vue
+++ src/components/transactionList.vue
@@ -1,21 +1,20 @@
 <script setup lang="ts">
-const transactions = [
-  { id: 1, text: 'Flower', amount: -19.99 },
-  { id: 2, text: 'Salary', amount: 299.97 },
-  { id: 3, text: 'Book', amount: -10 },
-  { id: 4, text: 'Camera', amount: 150 }
-]
+import type { transaction } from '@/App.vue'
+
+defineProps<{
+  transactions: transaction[]
+}>()
 </script>
 
 <template>
   <h3>History</h3>
   <ul id="list" class="list">
     <li
-      :class="transaction.amount >= 0 ? 'plus' : 'minus'"
+      :class="transaction.amount < 0 ? 'minus' : 'plus'"
       v-for="transaction in transactions"
       :key="transaction.id"
     >
-      {{ transaction.text }} <span>{{ transaction.amount }}</span
+      {{ transaction.text }} <span>${{ transaction.amount }}</span
       ><button class="delete-btn">x</button>
     </li>
   </ul>
